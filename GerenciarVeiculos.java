8d922efe9ca68e9d7fe8bc540045edbaa7832307
sothiis <21424597@anhembimorumbi.edu.br>
2021-04-24 16:57:32 -0300

    first commit

---
+++ GerenciarVeiculos.java
@@ -0,0 +1,26 @@
+public class GerenciarVeiculos {
+    public void adicionar (veiculo A) {
+
+    }
+
+    public boolean remover (String placa) {
+        return true;
+        //return false;
+    }
+
+    public Veiculo buscarPorPlaca (String Placa) {
+        return null;
+    }
+    public String listarVeiculos() {
+        //return String;
+    }
+
+    public double obterValorImposto(String placa){
+
+    }
+
+    public String listarVeiculoPorCombustivel(String tipoCombuString){
+
+    }
+    
+}
---
+++ Veiculo.java
@@ -0,0 +1,46 @@
+public class Veiculo {
+    public String modelo;
+    public String marca;
+    public int anoFabricacao;
+    public double valorMercado;
+    public String placa;
+    public String tipoCombustivel;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getAnofabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setValordeMercado(double valorMercado) {
+        this.valorMercado = valorMercado;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getTipocombustivel() {
+        return tipoCombustivel;
+    }
+
+    public double calcularImposto() {
+        /* Carros com mais de 20 anos são isentos (ou seja, impostoé zero)
+        Carros movidos a gasolina, diesel e flex devem pagar4% do valor de mercado como imposto
+        Demais combustíveis: imposto é 3% do valor de mercado
+         */
+    }
+
+    public String imprimir() {
+
+    }
+
+
+  
+}
